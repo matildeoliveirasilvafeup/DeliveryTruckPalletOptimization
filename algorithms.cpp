f79565997925840ffd2e666dbf90efff44399bc2
Camila Silva <up202305458@up.pt>
2025-05-20 11:26:53 +0100

    added back tracking algorithm

--- algorithms.cpp
+++ algorithms.cpp
@@ -37,3 +37,36 @@ unsigned int Algorithms::brute_force(unsigned int values[], unsigned int weights
     }
     return maxValue;
 }
+
+unsigned int Algorithms::back_tracking(unsigned int values[], unsigned int weights[], unsigned int n, unsigned int maxWeight, bool usedItems[]) {
+    unsigned int maxValue = 0;
+    unsigned int curIndex = 0; //identifies the current lv on the tree
+    bool curItem[n];
+    for (int i = 0; i < n; i++) {
+        curItem[i] = false;
+        usedItems[i] = false;
+    }
+    back_tracking_rec(values, weights, n, curIndex, maxWeight, 0, curItem, maxValue, usedItems);
+
+    return maxValue;
+}
+
+void Algorithms::back_tracking_rec(unsigned int values[], unsigned int weights[], unsigned int n, unsigned int curIndex, unsigned int maxWeight, unsigned int curValue, bool curItems[], unsigned int& maxValue, bool usedItems[]) {
+    //leaf node
+    if (curIndex == n) {
+        if (curValue > maxValue) {
+            maxValue = curValue;
+            for (int i = 0; i < n; i++) usedItems[i] = curItems[i];
+        }
+    }
+    else {
+        //option 1 - include node
+        if (weights[curIndex] <= maxWeight) {
+            curItems[curIndex] = true;
+            back_tracking_rec(values, weights, n, curIndex+1, maxWeight - weights[curIndex], curValue + values[curIndex], curItems, maxValue, usedItems);
+            curItems[curIndex] = false;
+        }
+        //option 2 - don't include
+        back_tracking_rec(values, weights, n, curIndex + 1, maxWeight, curValue, curItems, maxValue, usedItems);
+    }
+}
