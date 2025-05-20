7e98587ac3cd06f907dca37ffb08fc10abcc7012
Camila Silva <up202305458@up.pt>
2025-05-20 11:35:54 +0100

    ilp declaration

--- algorithms.h
+++ algorithms.h
@@ -8,7 +8,7 @@ class Algorithms {
 public:
     static unsigned int brute_force(unsigned int values[], unsigned int weights[], unsigned int n, unsigned int maxWeight, bool usedItems[]);
     static unsigned int back_tracking(unsigned int values[], unsigned int weights[], unsigned int n, unsigned int maxWeight, bool usedItems[]);
-
+    static unsigned int ilp(unsigned int values[], unsigned int weights[], unsigned int n, unsigned int maxWeight, bool usedItems[]);
 private:
     static void back_tracking_rec(unsigned int values[], unsigned int weights[], unsigned int n, unsigned int curIndex, unsigned int maxWeight, unsigned int curValue, bool curItems[], unsigned int& maxValue, bool usedItems[]);
 };
