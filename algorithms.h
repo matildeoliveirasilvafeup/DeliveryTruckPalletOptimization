11f19713349819ea23cf286bd2912ddb74ec7017
Camila <up202305458@up.pt>
2025-05-23 21:10:28 +0100

    base documentation for each algorithm

--- algorithms.h
+++ algorithms.h
@@ -9,13 +9,53 @@
 #define ALGORITHMS_H
 class Algorithms {
 public:
+    ///
+    /// @param values unsigned int[] - an array with the value of each pallet
+    /// @param weights unsigned int[] - an array with the weight of each pallet
+    /// @param n unsigned int - number of pallets
+    /// @param maxWeight unsigned int - maximum weight the delivery truck can carry
+    /// @param usedItems bool[] - an array indicating whether a pallet is used in the solution (true) or not (false)
+    /// @return unsigned int - the total value achieved with the combination of pallets the algorithm chose
     static unsigned int brute_force(unsigned int values[], unsigned int weights[], unsigned int n, unsigned int maxWeight, bool usedItems[]);
+
+    ///
+    /// @param values unsigned int[] - an array with the value of each pallet
+    /// @param weights unsigned int[] - an array with the weight of each pallet
+    /// @param n unsigned int - number of pallets
+    /// @param maxWeight unsigned int - maximum weight the delivery truck can carry
+    /// @param usedItems bool[] - an array indicating whether a pallet is used in the solution (true) or not (false)
+    /// @return unsigned int - the total value achieved with the combination of pallets the algorithm chose
     static unsigned int back_tracking(unsigned int values[], unsigned int weights[], unsigned int n, unsigned int maxWeight, bool usedItems[]);
+
+    ///
+    /// @param values unsigned int[] - an array with the value of each pallet
+    /// @param weights unsigned int[] - an array with the weight of each pallet
+    /// @param n unsigned int - number of pallets
+    /// @param maxWeight unsigned int - maximum weight the delivery truck can carry
+    /// @param usedItems bool[] - an array indicating whether a pallet is used in the solution (true) or not (false)
+    /// @return unsigned int - the total value achieved with the combination of pallets the algorithm chose
     static unsigned int ilp(unsigned int values[], unsigned int weights[], unsigned int n, unsigned int maxWeight, bool usedItems[]);
+
+    ///
+    /// @param values unsigned int[] - an array with the value of each pallet
+    /// @param weights unsigned int[] - an array with the weight of each pallet
+    /// @param n unsigned int - number of pallets
+    /// @param maxWeight unsigned int - maximum weight the delivery truck can carry
+    /// @param usedItems bool[] - an array indicating whether a pallet is used in the solution (true) or not (false)
+    /// @return unsigned int - the total value achieved with the combination of pallets the algorithm chose
     static unsigned int dynamic(unsigned int values[], unsigned int weights[], unsigned int n, unsigned int maxWeight, bool usedItems[]);
 private:
+    ///
+    /// @param values unsigned int[] - an array with the value of each pallet
+    /// @param weights unsigned int[] - an array with the weight of each pallet
+    /// @param n unsigned int - number of pallets
+    /// @param curIndex unsigned int - the current index that's being explored in the decision tree
+    /// @param maxWeight unsigned int - maximum weight the delivery truck can carry
+    /// @param curValue unsigned int - the total value of the pallets in the current decision state
+    /// @param curItems bool[] - an array indicating which pallets are being considered in the current decision state
+    /// @param maxValue unsigned int - the overall maximum value that the algorithm found in the decision states it has explored
+    /// @param usedItems bool[] - an array indicating whether a pallet is used in the solution (true) or not (false)
     static void back_tracking_rec(unsigned int values[], unsigned int weights[], unsigned int n, unsigned int curIndex, unsigned int maxWeight, unsigned int curValue, bool curItems[], unsigned int& maxValue, bool usedItems[]);
-	static std::pair<int, std::vector<int>> solveWithDP(std::vector<Pallet>& pallets, int capacity); //added by Matilde
 };
 
 #endif //ALGORITHMS_H
